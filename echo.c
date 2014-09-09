d9a30d317f1752023febfc3e64263ef6dcacfaa4
anongnix <ginxos@hush.ai>
2014-09-09 15:42:39 +0100

    Update echo.c

--- echo.c
+++ echo.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  int i;
-  for (i = 1; i < argc; i++) {
-    printf("%s%s", argv[i], " ");
+  for (int i = 1; i < argc; i++) {
+    printf("%s", argv[i]);
   }
   printf("\n");
-  return 0;
 }
